5a3e2f042e98b117f91e6703541335dad45bbde2
met44 <gkehren@gmail.com>
2019-06-29 14:19:45 +0200

    Moved USO core into "uStableObject" subfolder
    preparation for integration with utilities

--- uStableObject/EventListeners/Wrappers/GameEventListenerWrapper.cs
+++ uStableObject/EventListeners/Wrappers/GameEventListenerWrapper.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace                   uStableObject.Data
 {

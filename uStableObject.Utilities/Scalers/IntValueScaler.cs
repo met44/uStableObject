0a7e4e1857e6f4b1c5a788bae69d1496010e56dc
met44 <gkehren@gmail.com>
2019-07-09 15:19:01 +0200

    Added IntValueScaler to scale an int value and emit the new value through unity event
    Can be used to tweak a value for display in a UI

---
+++ uStableObject.Utilities/Scalers/IntValueScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using uStableObject.Utilities;
+
+namespace                                       uStableObject.Utilities.Scalers
+{
+    public class                                IntValueScaler : MonoBehaviour
+    {
+        #region Input Data
+        [SerializeField] UnityEventTypes.Int    _ouputValue;
+        [SerializeField] float                  _scale;
+        #endregion
+
+        #region Triggers
+        public void                             ScaleValue(int val)
+        {
+            this._ouputValue.Invoke(Mathf.RoundToInt(val * this._scale));
+        }
+        #endregion
+    }
+}

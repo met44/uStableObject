d240e6b04085d9c81fa6e1fff453065efd01910b
Guillaume Kehren <gkehren@gmail.com>
2019-07-17 19:22:45 +0200

    Added Var emitter base + Vector2Int implementation

---
+++ uStableObject.Utilities/Emitters/VarEmitterBase.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using uStableObject.Data;
+
+namespace                                       uStableObject.Utilities
+{
+    public class                                VarEmitterBase<T, E, V> : MonoBehaviour 
+                                                where E : UnityEvent<T> 
+                                                where V : IBaseTypeVar<T>
+    {
+        #region Input Data
+        [SerializeField] E                      _onEmit;
+        [SerializeField] V                      _var;
+        #endregion
+
+        #region Members
+        #endregion
+
+        #region Triggers
+        public void                             Emit(T val)
+        {
+            this._onEmit.Invoke(val);
+        }
+
+        public void                             EmitMemorized()
+        {
+            this._onEmit.Invoke(this._var.Value);
+        }
+
+        public void                             SetVar(V val)
+        {
+            this._var = val;
+        }
+        #endregion
+    }
+}
---
+++ uStableObject.Utilities/Emitters/Vector2IntVarEmitter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using uStableObject.Data;
+
+namespace                                       uStableObject.Utilities.Emitter
+{
+    public class                                Vector2IntVarEmitter : VarEmitterBase<Vector2Int, UnityEventTypes.V2Int, Vector2IntVar>
+    {
+    }
+}

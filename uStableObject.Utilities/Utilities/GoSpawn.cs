e53a6acb772ca9ec2b4d28f0fa6366d325a2ddf4
Guillaume Kehren <gkehren@gmail.com>
2019-08-12 12:22:24 +0200

    Added GoSpawn component
    spawns an object through GoPool at the current location of the calling transform

---
+++ uStableObject.Utilities/Utilities/GoSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uStableObject.Utilities;
+
+namespace                               uStableObject.Utilities
+{
+    public class                        GoSpawn : MonoBehaviour
+    {
+        #region Input Data
+        [SerializeField] GameObject     _prefab;
+        [SerializeField] Transform      _parent;
+        #endregion
+
+        #region Triggers
+        public void                     SpawnNow()
+        {
+            GoPool.Spawn(this._prefab, this.transform.position, this.transform.rotation, this._parent);
+        }
+        #endregion
+    }
+}

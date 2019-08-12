cd5ce354d787953cb54bc16f10d8f100af8f458b
Guillaume Kehren <gkehren@gmail.com>
2019-08-12 17:04:41 +0200

    Added Rand float & int emitters

---
+++ uStableObject.Utilities/RandomEmitters/RandFloatEmitter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace                                       uStableObject.Utilities
+{
+    public class                                RandFloatEmitter : FloatEmitter
+    {
+        #region Input Data
+        [SerializeField] float                  _randMin;
+        [SerializeField] float                  _randMax;
+        #endregion
+
+        #region Members
+        #endregion
+
+        #region Triggers
+        public void                             EmitRand()
+        {
+            this.Emit(Random.Range(this._randMin, this._randMax));
+        }
+        #endregion
+    }
+}
---
+++ uStableObject.Utilities/RandomEmitters/RandIntEmitter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace                                       uStableObject.Utilities
+{
+    public class                                RandIntEmitter : IntEmitter
+    {
+        #region Input Data
+        [SerializeField] int                    _randMin;
+        [SerializeField] int                    _randMax;
+        #endregion
+
+        #region Members
+        #endregion
+
+        #region Triggers
+        public void                             EmitRand()
+        {
+            this.Emit(Random.Range(this._randMin, this._randMax));
+        }
+        #endregion
+    }
+}

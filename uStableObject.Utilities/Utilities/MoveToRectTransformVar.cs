76804416ce5119d397759470d7786c084c6a6372
Guillaume Kehren <gkehren@gmail.com>
2019-08-13 18:07:18 +0200

    Added position option to MoveToRectTransform to chose which part is targeted

--- uStableObject.Utilities/Utilities/MoveToRectTransformVar.cs
+++ uStableObject.Utilities/Utilities/MoveToRectTransformVar.cs
@@ -12,6 +12,7 @@ namespace                                   uStableObject.Utilities
         [SerializeField] CanvasVar          _canvas;
         [SerializeField] RectTransformVar   _target;
         [SerializeField] Modes              _mode;
+        [SerializeField] Positions          _position;
         #endregion
 
         #region Trigger
@@ -19,7 +20,22 @@ namespace                                   uStableObject.Utilities
         {
             if (this._mode == Modes.CanvasToCameraLocal)
             {
-                Vector3 position = this._camera.Camera.CanvasToCameraPoint(this._canvas.Canvas, this._target.Transform.position, this.transform.localPosition.z);
+                Vector3                     localPos = Vector3.zero;
+
+                switch (this._position)
+                {
+                    case Positions.TopLeft:         localPos = new Vector3(this._target.Transform.rect.xMin,    this._target.Transform.rect.yMax,       this._target.Transform.localPosition.z); break;
+                    case Positions.TopCenter:       localPos = new Vector3(this._target.Transform.rect.center.x,this._target.Transform.rect.yMax,       this._target.Transform.localPosition.z); break;
+                    case Positions.TopRight:        localPos = new Vector3(this._target.Transform.rect.xMax,    this._target.Transform.rect.yMax,       this._target.Transform.localPosition.z); break;
+                    case Positions.MiddleLeft:      localPos = new Vector3(this._target.Transform.rect.xMin,    this._target.Transform.rect.center.y,   this._target.Transform.localPosition.z); break;
+                    case Positions.MiddleCenter:    localPos = new Vector3(this._target.Transform.rect.center.x,this._target.Transform.rect.center.y,   this._target.Transform.localPosition.z); break;
+                    case Positions.MiddleRight:     localPos = new Vector3(this._target.Transform.rect.xMax,    this._target.Transform.rect.center.y,   this._target.Transform.localPosition.z); break;
+                    case Positions.BottomLeft:      localPos = new Vector3(this._target.Transform.rect.xMin,    this._target.Transform.rect.yMin,       this._target.Transform.localPosition.z); break;
+                    case Positions.BottomCenter:    localPos = new Vector3(this._target.Transform.rect.center.x,this._target.Transform.rect.yMin,       this._target.Transform.localPosition.z); break;
+                    case Positions.BottomRight:     localPos = new Vector3(this._target.Transform.rect.xMax,    this._target.Transform.rect.yMin,       this._target.Transform.localPosition.z); break;
+                }
+                Vector3 worldPos = this._target.Transform.TransformPoint(localPos);
+                Vector3 position = this._camera.Camera.CanvasToCameraPoint(this._canvas.Canvas, worldPos, this.transform.localPosition.z);
                 this.transform.localPosition = position;
             }
         }
@@ -29,6 +45,11 @@ namespace                                   uStableObject.Utilities
         public enum                         Modes
         {
             CanvasToCameraLocal
+        }
+
+        public enum                         Positions
+        {
+            TopLeft, TopCenter, TopRight, MiddleLeft, MiddleCenter, MiddleRight, BottomLeft, BottomCenter, BottomRight
         }
         #endregion
     }

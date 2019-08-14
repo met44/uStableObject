c89e6d9fecc6125893951e156e2851071409b967
Guillaume Kehren <gkehren@gmail.com>
2019-08-14 10:33:14 +0200

    code cleanup

--- uStableObject/Events/GameEventData.cs
+++ uStableObject/Events/GameEventData.cs
@@ -1,17 +1,23 @@
-﻿using uStableObject.Data;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using uStableObject.Data;
 
 namespace                           uStableObject
 {
     public abstract class           GameEventData : BoolVar
     {
+        #region Input Data
         [SerializeField] UnityEvent _changedEvent;
-        public UnityEvent           Changed { get { return (this._changedEvent); } }
+        #endregion
 
+        #region Properties
+        public UnityEvent           Changed { get { return (this._changedEvent); } }
+        #endregion
+
+        #region Unity
         protected virtual void      OnEnable()
         {
             if (this._changedEvent == null)
@@ -21,9 +27,14 @@ namespace                           uStableObject
             this._changedEvent.RemoveAllListeners();
             this._changedEvent.AddListener(this.RaiseIsNotNull);
         }
+        #endregion
 
-        protected abstract void     RaiseIsNotNull();
-
+        #region Triggers
         public abstract void        Clear();
+        #endregion
+
+        #region Helpers
+        protected abstract void     RaiseIsNotNull();
+        #endregion
     }
 }

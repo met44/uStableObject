cbea82a46fe7351e23f3d4cd43b37b8d569741d5
Guillaume Kehren <gkehren@gmail.com>
2019-08-20 17:36:48 +0200

    Added "Interval" data type, works in inspector (unlike RangeInt) and has a property to get a random value in the interval

---
+++ uStableObject.Utilities/Utilities/Editor/IntervalDrawer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace                       uStableObject.Utilities
+{
+    [CustomPropertyDrawer(typeof(Interval))]
+    public class                IntervalDrawer : PropertyDrawer
+    {
+        public override float   GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 16;
+        }
+
+        public override void    OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            var fromProperty = property.FindPropertyRelative("_from");
+            var toProperty = property.FindPropertyRelative("_to");
+            position.width *= .41f;
+            EditorGUI.LabelField(position, label);
+            position.width /= 2;
+            position.x = position.width * 2;
+            EditorGUI.PropertyField(position, fromProperty, GUIContent.none);
+            position.x += position.width;
+            EditorGUI.PropertyField(position, toProperty, GUIContent.none);
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ uStableObject.Utilities/Utilities/Interval.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace                       uStableObject.Utilities
+{
+    [System.Serializable]
+    public class                Interval
+    {
+        #region Input Data
+        [SerializeField] int    _from;
+        [SerializeField] int    _to;
+        #endregion
+
+        #region Properties
+        public int              From    { get { return (this._from); }  set { this._from = value; } }
+        public int              To      { get { return (this._to); }    set { this._to = value; } }
+        public int              Rand    { get { return (Random.Range(this._from, this._to)); } }
+        #endregion
+
+        #region Constructors
+        public                  Interval() { }
+        public                  Interval(int from, int to) { this._from = from; this._to = to; }
+        #endregion
+    }
+}

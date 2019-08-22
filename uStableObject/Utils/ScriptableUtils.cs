9d5690239d4c54e37e483e45f431b0a50d5f10e9
Guillaume Kehren <gkehren@gmail.com>
2019-08-22 18:22:57 +0200

    Added ScriptableUtils.AddInSameFolder() helper

--- uStableObject/Utils/ScriptableUtils.cs
+++ uStableObject/Utils/ScriptableUtils.cs
@@ -16,7 +16,7 @@ namespace                               uStableObject.Utilities
         }
 
         //[ContextMenu("Add Event Listener Child")]
-        public static T                 AddAsChild<T>(string name, Object parent = null) where T : ScriptableObject
+        public static T                 AddAsChild<T>(string name, Object parent = null, bool autoSelect = true) where T : ScriptableObject
         {
             T                           child = default(T);
 #if UNITY_EDITOR
@@ -29,10 +29,45 @@ namespace                               uStableObject.Utilities
                 UnityEditor.AssetDatabase.AddObjectToAsset(child, data);
                 UnityEditor.EditorUtility.SetDirty(data);
                 UnityEditor.AssetDatabase.SaveAssets();
-                UnityEditor.Selection.activeObject = child;
+                if (autoSelect)
+                {
+                    UnityEditor.Selection.activeObject = child;
+                }
+                else
+                {
+                    UnityEditor.EditorGUIUtility.PingObject(child);
+                }
             }
 #endif
             return (child);
+        }
+
+        //[ContextMenu("Add Event Listener Child")]
+        public static T                 AddInSameFolder<T>(string name, Object parent = null, bool autoSelect = true) where T : ScriptableObject
+        {
+            T                           newInstance = default(T);
+#if UNITY_EDITOR
+
+            if (UnityEditor.Selection.activeObject is ScriptableObject)
+            {
+                string path = UnityEditor.AssetDatabase.GetAssetPath(parent);
+                path = path.Substring(0, path.LastIndexOf("/") + 1);
+                path = UnityEditor.AssetDatabase.GenerateUniqueAssetPath(path + name + ".asset");
+                newInstance = ScriptableObject.CreateInstance<T>();
+                newInstance.name = name;
+                UnityEditor.AssetDatabase.CreateAsset(newInstance, path);
+                UnityEditor.AssetDatabase.SaveAssets();
+                if (autoSelect)
+                {
+                    UnityEditor.Selection.activeObject = newInstance;
+                }
+                else
+                {
+                    UnityEditor.EditorGUIUtility.PingObject(newInstance);
+                }
+            }
+#endif
+            return (newInstance);
         }
     }
 }

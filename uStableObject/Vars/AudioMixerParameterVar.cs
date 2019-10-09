4c29bd5729a95259ccbafa1ee353a9ba1b3f994e
Guillaume Kehren <gkehren@gmail.com>
2019-10-10 01:30:57 +0200

    Added proper default value to AudioMixer var

--- uStableObject/Vars/AudioMixerParameterVar.cs
+++ uStableObject/Vars/AudioMixerParameterVar.cs
@@ -9,19 +9,25 @@ namespace                               uStableObject.Data
     [CreateAssetMenu(menuName = "uStableObject/Settings/AudioMixer Parameter Var", order = 1)]
     public class                        AudioMixerParameterVar : FloatVar, IGameEventListener<float>
     {
+        #region Input Data
         [SerializeField] string         _parameter;
         [SerializeField] AudioMixer     _target;
         [SerializeField] Modes          _mode;
-        [SerializeField] float          _scale;
+        [SerializeField] float          _scale = 20;
         [SerializeField] float          _offset;
+        #endregion
 
+        #region Properties
         public bool                     Editing { get; set; }
+        #endregion
 
+        #region Unity
         private void                    OnEnable()
         {
             this.Register(this);
             (this as IGameEventListener<float>).OnEventRaised(this.Value);
         }
+        #endregion
 
         void IGameEventListener<float>.OnEventRaised(float param)
         {
@@ -32,9 +38,11 @@ namespace                               uStableObject.Data
             this._target.SetFloat(this._parameter, param * this._scale + this._offset);
         }
 
+        #region Data Types
         public enum                     Modes
         {
             Log, Linear
         }
+        #endregion
     }
 }

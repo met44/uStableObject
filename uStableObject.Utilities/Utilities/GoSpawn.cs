162416e8a748ae7ed2b03e362b2a6752e5762126
Guillaume Kehren <gkehren@gmail.com>
2020-01-27 23:55:05 +0100

    GoSpawn can now use a TransformVar parameter instead of direct transform ref

--- uStableObject.Utilities/Utilities/GoSpawn.cs
+++ uStableObject.Utilities/Utilities/GoSpawn.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using uStableObject.Data;
 using uStableObject.Utilities;
 
 namespace                               uStableObject.Utilities
@@ -10,12 +11,13 @@ namespace                               uStableObject.Utilities
         #region Input Data
         [SerializeField] GameObject     _prefab;
         [SerializeField] Transform      _parent;
+        [SerializeField] TransformVar   _parentVar;
         #endregion
 
         #region Triggers
         public void                     SpawnNow()
         {
-            GoPool.Spawn(this._prefab, this.transform.position, this.transform.rotation, this._parent);
+            GoPool.Spawn(this._prefab, this.transform.position, this.transform.rotation, this._parent ? this._parent : this._parentVar.Transform);
         }
         #endregion
     }

d288818138c2b3e4a706b957b17f8be595634278
Guillaume Kehren <gkehren@gmail.com>
2020-03-23 16:12:31 +0100

    New helper to instantiate things using unity events

---
+++ uStableObject.Utilities/Utilities/InstantiateThis.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace                   uStableObject.Utilities
+{
+    //helper to trigger from unity events
+    public class            InstantiateThis : MonoBehaviour
+    {
+        public void         InstantiateNow()
+        {
+            Instantiate(this.gameObject, this.transform.position, this.transform.rotation);
+        }
+    }
+}

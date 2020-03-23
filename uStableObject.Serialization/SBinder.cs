2472850e30652803127d7a62d6e48a99e5489a7e
Guillaume Kehren <gkehren@gmail.com>
2020-03-23 16:08:35 +0100

    New serialization helper API

---
+++ uStableObject.Serialization/SBinder.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace                                       uStableObject.Data
+{
+    [System.Serializable]
+    public static class                         SBinder<T> 
+                                                where T : IObjectID
+    {
+        #region Members
+        static Dictionary<uint, T>              _refs= new Dictionary<uint, T>();
+        #endregion
+
+        #region Triggers
+        public static void                      Clear()
+        {
+            _refs.Clear();
+        }
+
+        //Check whether is either free or pointing to the right object
+        public static bool                      CheckIDValidity(T obj)
+        {
+            T                                   existingRef = default(T);
+
+            _refs.TryGetValue(obj.ID, out existingRef);
+            return (obj.ID > 0 && (existingRef == null || existingRef.Equals(obj)));
+        }
+
+        public static uint                      GetAvailableID()
+        {
+            uint highestId = 0;
+            foreach (var key in _refs.Keys)
+            {
+                if (key > highestId)
+                {
+                    highestId = key;
+                }
+            }
+            return (highestId + 1);
+        }
+
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void                      EnsureUniqueID(T obj)
+        {
+#if UNITY_EDITOR
+            if (!CheckIDValidity(obj))
+            {
+                FieldInfo                       idField = null;
+
+                var type = obj.GetType();
+                while (idField == null)
+                {
+                    idField = type.GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
+                    type = type.BaseType;
+                    if (type.BaseType == typeof(System.Object))
+                    {
+                        throw new System.Exception("_id field not found");
+                    }
+                }
+                uint newId = GetAvailableID();
+                idField.SetValue(obj, newId);
+                if (obj is Object)
+                {
+                    UnityEditor.EditorUtility.SetDirty(obj as Object);
+                }
+            }
+#endif
+        }
+
+        public static void                      Init(T obj)
+        {
+            if (_refs.ContainsKey(obj.ID))
+            {
+                _refs[obj.ID] = obj;
+            }
+            else
+            {
+                _refs.Add(obj.ID, obj);
+            }
+        }
+
+        public static T                         Get(uint id)
+        {
+            if (_refs.TryGetValue(id, out T output))
+            {
+                return (output);
+            }
+            return (default(T));
+        }
+        #endregion
+    }
+}
---
+++ uStableObject.Serialization/SProxy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace                                       uStableObject.Data
+{
+    [System.Serializable]
+    public class                                SProxy<T> 
+                                                where T : IObjectID
+    {
+        #region Members
+        [SerializeField] protected uint         _id;
+        #endregion
+
+        #region Properties
+        public uint                             ID                  => this._id;
+        private T                               Instance           { get; set; }
+        public static implicit operator         T(SProxy<T> proxy)  => proxy.Instance == null ? (proxy.Instance = SBinder<T>.Get(proxy._id)) : proxy.Instance;
+
+        //implicit cast version is sexier but unity wont serialize the generic version :/
+        public static P                         Make<P>(T obj) where P : SProxy<T>, new()
+        {
+            return (new P() { _id = obj.ID, Instance = obj });
+        }
+        #endregion
+    }
+}
